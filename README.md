7b0ceb34a8683d1c93f27223d31c17bfe43bd3e4
savokiss <jaynaruto@qq.com>
2019-04-21 21:57:27 +0800

    fix(readme): template path

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Dead simple Vue preset template for learning use.
 
 # Usage
 Just clone it and edit.
+
+# Posts
+- [10分钟搭建属于自己的 Vue CLI3 项目模板](https://savokiss.com/tech/vue-cli3-preset.html)
--- generator/index.js
+++ generator/index.js
@@ -7,13 +7,15 @@ module.exports = (api, options, rootOptions) => {
   })
 
   // 复制并用 ejs 渲染 `./template` 内所有的文件
-  api.render('./template')
+  api.render('../template')
 
   if (options.module === 'module1') { 
     // options.module 可以访问上面问题数组的第一个对象的值，默认为: 'module0'
+    console.log(`Your choice is ${options.module}`)
   }
 
   if (options.moduleName === 'myModule') {
     // options.moduleName 可以访问到用户从控制台输入的文字
+    console.log(`Your input is ${options.moduleName}`)
   }
 }
